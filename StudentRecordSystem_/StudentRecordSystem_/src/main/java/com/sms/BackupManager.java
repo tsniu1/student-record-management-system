3ee32351b6bbef689cc9fbf78a5f6e0fd3b66c08
tsniu1 <tsinuel.abrham@bitscollege.edu.et>
2026-06-17 20:16:06 +0300

    Update BackupManager.java

--- StudentRecordSystem_/StudentRecordSystem_/src/main/java/com/sms/BackupManager.java
+++ StudentRecordSystem_/StudentRecordSystem_/src/main/java/com/sms/BackupManager.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-
 public class BackupManager {
 
     private static final SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
